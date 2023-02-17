21e6859076206d26ed11509b8547e28c5e7680d0
EverybodyLies-EverybodyDies <113132956+EverybodyLies-EverybodyDies@users.noreply.github.com>
2023-02-17 21:39:48 +0300

    Add files via upload

---
+++ fielname.js
@@ -0,0 +1,9 @@
+let checkButton = document.getElementById("check");
+checkButton.addEventListener("click", check);
+
+function check() {
+    let fielname = document.getElementById("fielname").value;
+    let reg = /^[A-Za-z]\w\.(jpg|png|gif)/;
+alert(document.getElementById("answer").innerHTML = "true");
+}
+
