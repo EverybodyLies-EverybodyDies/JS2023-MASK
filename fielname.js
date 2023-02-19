4a838c3d2c093a0828d021a906e4be9d94d6899c
EverybodyLies-EverybodyDies <113132956+EverybodyLies-EverybodyDies@users.noreply.github.com>
2023-02-19 22:38:57 +0300

    Add files via upload

--- fielname.js
+++ fielname.js
@@ -4,6 +4,8 @@ checkButton.addEventListener("click", check);
 function check() {
     let fielname = document.getElementById("fielname").value;
     let reg = /^[A-Za-z]\w\.(jpg|png|gif)/;
+    let result = fielname.match(reg);
 alert(document.getElementById("answer").innerHTML = "true");
+
 }
 
